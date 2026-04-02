31d32448954ccbd29048d8211f31177a6362a3b1
vismaychuriwala <vismaychuriwala@gmail.com>
2026-04-02 16:43:52 -0400

    reverting entropy

--- src/isaac_quad_sim2real/tasks/race/config/crazyflie/agents/rsl_rl_ppo_cfg.py
+++ src/isaac_quad_sim2real/tasks/race/config/crazyflie/agents/rsl_rl_ppo_cfg.py
@@ -27,7 +27,7 @@ class QuadcopterPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.02,
+        entropy_coef=0.0,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=5.0e-4,
